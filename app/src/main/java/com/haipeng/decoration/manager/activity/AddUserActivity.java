eccbf06c3407b0d5f89b77b8bd624e19cfcb2bdc
孙宜岩 <yysun@wanin.net>
2017-07-28 16:11:48 +0800

    first commit

---
+++ app/src/main/java/com/haipeng/decoration/manager/activity/AddUserActivity.java
@@ -0,0 +1,15 @@
+package com.haipeng.decoration.manager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.haipeng.decoration.manager.R;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
